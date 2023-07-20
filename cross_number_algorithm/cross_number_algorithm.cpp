fb752223e1af90acde154b3b61c1f628f3b98108
kengo519 <kenjey.unity@gmail.com>
2023-07-20 23:06:22 +0900

    <fix> cross number algorithmのPython版を追加

--- cross_number_algorithm/cross_number_algorithm.cpp
+++ cross_number_algorithm/cross_number_algorithm.cpp
@@ -18,8 +18,11 @@ class Polygon {
   explicit Polygon(const std::vector<Point>& points) : vertexes(points) {}
 }
 
-// 点が多角形内にあるか判定する関数
-// true/false (多角形内/多角形外)
+/**
+ * 点が多角形内にあるか判定する関数
+ * 
+ * @return true(内側) / false(外側)
+ */
 bool is_point_inside_polygon(const Point& point, const Polygon& polygon) {
   // 交差回数のカウンタ
   uint32_t c = 0;
@@ -34,14 +37,33 @@ bool is_point_inside_polygon(const Point& point, const Polygon& polygon) {
     if (((v.y <= point.y) && (v1.y > point.y)) ||
         ((v.y > point.y) && (v1.y <= point.y))) {
       double x = v.x + ((point.y - v.y) / double(v1.y - v.y) * (v1.x - v.x));
+      
+      // 辺上にある場合は内側判定
+      if(point.x == x){
+        return true;
+      }
 
-      if(point.x < x){
+      if (point.x < x) {
         count++;
       }
     }
   }
 
-  return false;
+  return (count % 2) == 1;
 }
 
-int main() { return 0; }
+// 使用例
+int main() {
+  std::vector<Point> ps = {{1, 1}, {4, 1}, {4, 4}, {1, 4}};
+  Polygon polygon{ps};
+
+  Point p1{2, 2};
+
+  if (is_point_inside_polygon(p1, polygon)) {
+    std::cout << "inside!" << std::endl;
+  } else {
+    std::cout << "outside!" << std::endl;
+  }
+
+  return 0;
+}
---
+++ cross_number_algorithm/cross_number_algorithm.py
@@ -0,0 +1,35 @@
+def is_point_in_polygon(point, polygon):
+    # 交差回数のカウンタ
+    count = 0
+
+    p_x = point[0]
+    p_y = point[1]
+
+    for i, v, in enumerate(polygon):
+        # 頂点座標
+        v_x = v[0]
+        v_y = v[1]
+        # 隣の頂点座標
+        v1_x = polygon[0][0] if i == len(polygon)-1 else polygon[i+1][0]
+        v1_y = polygon[0][1] if i == len(polygon)-1 else polygon[i+1][1]
+
+        if((v_y <= p_y) and (v1_y > p_y)) or ((v_y > p_y) and (v1_y <= p_y)):
+            x = v_x + ((p_y - v_y) / (v1_y - v_y)*(v1_x -v_x))
+            # 辺上にある場合は内側判定
+            print((v1_y - v_y)*(v1_x -v_x))
+            if x == p_x:
+                return True
+            elif p_x < x:
+                count += 1
+
+    return bool(count%2 == 1)
+
+# 使用例
+if __name__ == '__main__':
+    point = [2,2]
+    polygon = [[1, 1], [4, 1], [4, 4], [1, 4]]
+
+    if is_point_in_polygon(point, polygon):
+        print("inside!")
+    else:
+        print("outside")
